dbb017435f2e431a5e3819f45d78e018c08043ce
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2023-09-20 23:57:38 +0530

    Create delete-beg.cpp

---
+++ LinkList/delete-beg.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+struct node {
+    int data;
+    struct node *next;
+};
+node *head=nullptr ,*temp,*n;
+
+void insert_end(int value){
+    n = new node;
+    n->data=value;
+    n->next = nullptr;
+    if(head==nullptr){
+        head=n;
+    }
+    else{
+        temp=head;
+        while(temp->next !=nullptr){
+            temp=temp->next;
+        }
+        temp->next=n;
+    }
+}
+
+void delete_beg(){
+    if(head == nullptr){
+        cout<<"There's is no element"<<endl;
+    }
+    else{
+        temp=head;
+        head = head->next;
+        delete temp;
+    }
+}
+void display(){
+    temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+int main(){
+    insert_end(9);
+    insert_end(6);
+    insert_end(8);
+    delete_beg();
+    display();
+    return 0;
+}
